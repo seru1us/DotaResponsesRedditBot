863a58a5bca371032fed1714643db7b022bca4cd
Sean McCully <seancmccully@gmail.com>
2017-08-23 17:37:50 -0400

    Update dota_responses_account.py

--- dota_responses_account.py
+++ dota_responses_account.py
@@ -15,21 +15,18 @@ INVALID_CODE_ERR_MSG = 'Invalid access code'
 def get_reddit():
     """Method preparing the connection to Reddit API using OAuth."""
     reddit = praw.Reddit(user_agent=properties.USER_AGENT, client_id=properties.APP_ID, client_secret=properties.APP_SECRET, refresh_token=properties.APP_REFRESH_CODE)
-    #reddit.set_oauth_app_info(client_id=properties.APP_ID, client_secret=properties.APP_SECRET, redirect_uri=properties.APP_URI)
     return reddit
 
 
 def get_account():
     """Method preparing the account using Reddit API."""
     reddit = get_reddit()
-    #reddit.refresh_access_information(properties.APP_REFRESH_CODE)
     return reddit
 
 
 def generate_access_code(test=False):
     """Method used to generate the access code to Reddit API."""
     reddit = get_reddit()
-    #prawbm
     url = reddit.get_authorize_url('uniqueKey', properties.SCOPES, True)
     if test:
         return url
@@ -51,7 +48,3 @@ def get_access_information(access_code):
         return INVALID_CODE_ERR_MSG
     else:
         return access_information
-
-
-#reddit_account = get_account()
-#print(reddit_account.subreddit(properties.SUBREDDIT))
