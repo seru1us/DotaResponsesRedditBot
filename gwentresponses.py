6cadaa028a07370f0e58b40935d4e6d67684637f
Sean McCully <seancmccully@gmail.com>
2017-08-30 14:06:54 -0400

    Update gwentresponses.py

--- gwentresponses.py
+++ gwentresponses.py
@@ -1,250 +1,242 @@
 # coding=UTF-8
 
-"""Main module of the gwent 2 subreddit Heroes Responses Bot.
+"""Module used to create dictionaries requited for the script to work.
 
-The main body of the script is running in this file. The comments are loaded from the subreddit
-and the script checks if the comment is a response from gwent 2. If it is, a proper comment is
-prepared. The comment is posted as a reply to the original post on Reddit.
+Responses and urls to responses as mp3s are parsed from gwent 2 Wiki: http://gwent2.gamepedia.com/"""
 
-Proper logging is provided - saved to 2 files as standard output and errors.
-"""
-
-import traceback
 import os
-from datetime import datetime, date
-import sqlite3
-import logging
-from logging.handlers import TimedRotatingFileHandler
-import prawcore
-import gwent_responses_account as account
+import re
+import json
+from urllib.request import Request, urlopen
+from urllib import parse
+
+from bs4 import BeautifulSoup
+
 import gwent_responses_properties as properties
-from responses_wiki import gwent_wiki_parser as parser
 
 __author__ = 'Jonarzz'
 
+URL_BEGINNING = 'https://gwent.gamepedia.com/'
+URL_START = ('api.php?action=query&list=categorymembers&cmlimit=max&')
+URL_END = ('cmprop=title&format=json&cmtitle=Category:')
+CATEGORY = 'Audio'
 
 SCRIPT_DIR = os.path.dirname(__file__)
 
-RESPONSES_DB_CONN = sqlite3.connect(os.path.join(SCRIPT_DIR, 'responses.db'))
-RESPONSES_DB_CURSOR = RESPONSES_DB_CONN.cursor()
-COMMENTS_DB_CONN = sqlite3.connect(os.path.join(SCRIPT_DIR, 'comments.db'), detect_types=sqlite3.PARSE_DECLTYPES)
-COMMENTS_DB_CURSOR = COMMENTS_DB_CONN.cursor()
 
-logger = logging.getLogger("Rotating Log")
-logger.setLevel(logging.INFO)
-handler = TimedRotatingFileHandler(properties.LOG_FILENAME,
-                                   when='H',
-                                   interval=12,
-                                   backupCount=5)
-logger.addHandler(handler)
+def generate_dictionaries(responses_filename, heroes_filename, shitty_wizard_filename):
+    """Method used to generate dictionaries for responses and hero names
+    (short, used in urls matched with full names)."""
+    responses, heroes, shitty_wizard = dictionary_of_responses(pages_for_category(CATEGORY))
+    json.dump(responses, open(os.path.join(SCRIPT_DIR, responses_filename), "w"))
+    json.dump(heroes, open(os.path.join(SCRIPT_DIR, heroes_filename), "w"))
+    json.dump(shitty_wizard, open(os.path.join(SCRIPT_DIR, shitty_wizard_filename), "w"))
 
 
-def add_flair_specific_response(comment, heroes_dict, response):
-    """ Will probably never use this crap, but in case /r/Gwent ends up goin that route I'll leave it."""
-    RESPONSES_DB_CURSOR.execute("SELECT id, img_dir FROM heroes WHERE css=?", [comment.author_flair_css_class])
-    hero_id_img = RESPONSES_DB_CURSOR.fetchone()
-    if hero_id_img:
-        RESPONSES_DB_CURSOR.execute("SELECT link FROM responses WHERE response=? AND hero_id=?", [response, hero_id_img[0]])
-        link = RESPONSES_DB_CURSOR.fetchone()
-        if link:
-            comment.reply(create_reply(link[0], heroes_dict, comment.body, hero_id_img[1]))
-            save_comment_id(comment.id, do_log=True)
-            return True
+def dictionary_from_file(filename):
+    """Method used to load a dictionary from text file with given name
+    (file contains JSON structure)."""
+    with open(os.path.join(SCRIPT_DIR, filename)) as file:
+        dictionary = json.load(file)
+        return dictionary
 
 
-def add_shitty_wizard_response(comment, heroes_dict, response):
-    """ Leftover function from the dota bot, but I honestly have no clue what it does."""
-    RESPONSES_DB_CURSOR.execute("SELECT link, hero_id FROM responses WHERE response=? AND hero IS NOT NULL ORDER BY RANDOM() LIMIT 1;", [response])
-    link_and_hero_id = RESPONSES_DB_CURSOR.fetchone()
-    RESPONSES_DB_CURSOR.execute("SELECT img_dir FROM heroes WHERE id=?", [link_and_hero_id[1]])
-    img_dir = RESPONSES_DB_CURSOR.fetchone()[0]
-    comment.reply(create_reply(link_and_hero_id[0], heroes_dict, comment.body, img_dir))
-    save_comment_id(comment.id, do_log=True)
-
-
-def add_regular_response(comment, heroes_dict, response):
-    """ Function that adds responses before adding them to comments."""
-    #RESPONSES_DB_CURSOR.execute("SELECT link, hero_id FROM responses WHERE response=? AND hero IS NOT NULL ORDER BY hero_id DESC, RANDOM() LIMIT 1", [response])
-    RESPONSES_DB_CURSOR.execute("SELECT link, hero FROM responses WHERE stripped=? AND hero IS NOT NULL ORDER BY hero_id DESC, RANDOM() LIMIT 1", [response])
-    link_and_hero_id = RESPONSES_DB_CURSOR.fetchone()
-    if link_and_hero_id:
-        #RESPONSES_DB_CURSOR.execute("SELECT img_dir FROM heroes WHERE id=?", [link_and_hero_id[1]])
-        #img_dir = RESPONSES_DB_CURSOR.fetchone()
-        #if img_dir:
-        #    comment.reply(create_reply(link_and_hero_id[0], heroes_dict, comment.body, img=img_dir[0]))
-        #    log("Added: " + comment.id)
-        #else:
-        #    comment.reply(create_reply(link_and_hero_id[0], heroes_dict, comment.body))
-        #    log("Added: " + comment.id)
-        comment.reply(create_reply(link_and_hero_id[0], heroes_dict, comment.body))
-        log("Added: " + comment.id)
-
-
-def prepare_specific_responses():
-    """ Not really used"""
-    output_dict = {}
-    output_dict["shitty wizard"] = add_shitty_wizard_response
-    return output_dict
-
-
-SPECIFIC_RESPONSES_DICT = prepare_specific_responses()
-
-
-def execute():
-    """Main method executing the script.
-
-    It connects to an account, loads dictionaries from proper files (declared in properties file).
-    Afterwards it executes add_comments method with proper arguments passed.
-    """
-    reddit_account = account.get_account()
-
-    #try:
-        #sticky = reddit_account.subreddit(properties.SUBREDDIT).sticky()
-    # Apparently this doesn't work, the prawcore
-    #except prawcore.exceptions.NotFound:
-    #    sticky = None
-    sticky = None
-    #log('START')
-
-    for submission in reddit_account.subreddit(properties.SUBREDDIT).new(limit=150):
-        add_comments_to_submission(submission, sticky)
-
-    for submission in reddit_account.subreddit(properties.SUBREDDIT).hot(limit=35):
-        add_comments_to_submission(submission, sticky)
-
-
-def add_comments_to_submission(submission, sticky):
-    """Method that adds the bot replies to the comments in the given submission."""
-    if submission == sticky:
-        return
-
-    heroes_dict = parser.dictionary_from_file(properties.HEROES_FILENAME)
-
-    add_comments(submission, heroes_dict)
-
-
-def add_message_to_file(message, filename):
-    """Method that appends given string message to a file with provided filename."""
-    with open(filename, 'a') as file:
-        file.write(str(datetime.now()) + '\n' + message + '\n')
-
-
-#def log(message, error=False):
-def log(message):
-    """ new logger with log rotating."""
-    #logger = logging.getLogger("Rotating Log")
-    #logger.setLevel(logging.INFO)
-
-    #handler = TimedRotatingFileHandler(properties.LOG_FILENAME,
-                                       #when='M',
-                                       #interval=5,
-                                       #backupCount=5)
-    #logger.addHandler(handler)
-    logger.info(str(datetime.now()) + '\n' + message + '\n')
-    #handler.stream.close()
-
-
-#def log(message, error=False):
-    #"""Method used to save messages to an proper (info/error) log file."""
-    #if error:
-    #    add_message_to_file(message, properties.ERROR_FILENAME)
-    #else:
-    #    add_message_to_file(message, properties.INFO_FILENAME)
-
-
-def add_comments(submission, heroes_dict):
-    """Method used to check all the comments in a submission and add replies if they are responses.
-
-    All comments are loaded. If comment ID is in the already doone comments database, next comment
-    is checked (further actions are ommited). If the comment wasn't analized before,
-    it is prepared for comparision to the responses in dictionary. If the comment is not on the
-    excluded responses list (loaded from properties) and if it is in the dictionary, a reply
-    comment is prepared and posted.
-    """
-    submission.comments.replace_more(limit=None, threshold=0)
-    for comment in submission.comments.list():
-        COMMENTS_DB_CURSOR.execute("SELECT id FROM comments WHERE id=?", [comment.id])
-        if COMMENTS_DB_CURSOR.fetchone():
+def create_responses_dict(ending):
+    """Method that for a given page ending creates a dictionary of pairs: response text-link."""
+    responses_dict = {}
+    list_of_responses = create_list_of_responses(ending)
+    for element in list_of_responses:
+        key = response_text_from_element(element)
+        if " " not in key:
             continue
 
-        stripped_body = comment.body.replace(r".", "")
-        stripped_body = stripped_body.replace(r",", "")
-        stripped_body = stripped_body.replace(r"'", "")
-        stripped_body = stripped_body.replace(r"’", "")
+        value = value_from_element(element)
 
-        #response = prepare_response(comment.body)
-        response = prepare_response(stripped_body)
-        if response in properties.EXCLUDED_RESPONSES:
-            save_comment_id(comment.id)
-            continue
+        if key not in responses_dict:
+            responses_dict[key] = value
 
-        #if add_flair_specific_response(comment, heroes_dict, response):
-            #continue
+    return responses_dict
 
-        if response in SPECIFIC_RESPONSES_DICT:
-            SPECIFIC_RESPONSES_DICT[response](comment, heroes_dict, response)
-            continue
+def dictionary_of_responses(category):
+    """Method that creates dictionaries - with the responses (response text - link to the file),
+    with hero names (short hero name used in Wiki files - long hero names),
+    with "shitty wizard" responses (hero name - link to the file).
 
-        add_regular_response(comment, heroes_dict, response)
-        save_comment_id(comment.id)
+    The dictionaries are created based on html body of a Wiki page related to the hero's responses.
+    Each response and hero name is prepared to be saved: stripped (unnecesary words/characters) and
+    turned to lowercase (only response text)."""
+    responses = {}
+    heroes = {}
+    shitty_wizard = {}
 
-
-def save_comment_id(comment_id, do_log=False):
-    """ This function is pretty important, saves the comment ID so we don't spam"""
-    if do_log:
-        log("Added: " + comment_id)
-    COMMENTS_DB_CURSOR.execute("INSERT INTO comments VALUES (?, ?)", (comment_id, date.today()))
-    COMMENTS_DB_CONN.commit()
-
-
-def create_reply(response_url, heroes_dict, orignal_text, img=None):
-    """Method that creates a reply in reddit-post format.
-
-    The message consists of a link the the response, the response itself, a warning about the sound
-    and an ending added from the properties file (post footer).
-    """
-    short_hero_name = parser.short_hero_name_from_actual_url(response_url)
-    hero_name = heroes_dict[short_hero_name]
-    if img:
-        return (
-            "[]({}): [{}]({}) (sound warning: {}){}"
-            .format(img, orignal_text, response_url, hero_name, properties.COMMENT_ENDING)
-            )
-    #else:
-    return (
-        "[{}]({}) (sound warning: {}){}"
-        .format(orignal_text, response_url, hero_name, properties.COMMENT_ENDING)
-        )
+    for ending in category:
+        print(ending)
+        list_of_responses = create_list_of_responses(ending)
+        for element in list_of_responses:
+            key = response_text_from_element(element)
+            if " " not in key:
+                continue
+            value = value_from_element(element)
+            short_hero = short_hero_name_from_url(element)
+            hero = short_hero
+            if short_hero not in heroes:
+                heroes[short_hero] = hero
+            if key == "shitty wizard":
+                if hero not in shitty_wizard:
+                    shitty_wizard[hero] = value
+            else:
+                if key not in responses:
+                    responses[key] = value
+    return responses, heroes, shitty_wizard
 
 
-def prepare_response(response):
-    """Method used to prepare  the response.
-
-    Dots and exclamation marks are stripped. The response is turned to lowercase.
-    Multiple letters ending the response are removed (e.g. ohhh->oh).
-    """
-    response = response.strip(" .!").lower()
-
-    #i = 1
-    new_response = response
-    # Temprarily removing this, is messing with a bunch of crap
-    #try:
-    #    while response[-1] == response[-1 - i]:
-    #        new_response = new_response[:-1]
-    #        i += 1
-    #except IndexError:
-    #    log("IndexError", True)
-
-    return new_response
+def create_list_of_responses(ending):
+    """ Grabs comment data from wiki. This isn't used currently."""
+    page_to_parse(URL_BEGINNING + ending)
+    page = page_to_parse(URL_BEGINNING + ending)
+    soup = BeautifulSoup(page, "html.parser")
+    list_of_responses = []
+    for element in soup.find_all("div", {"class" : "fullMedia"}):
+        if "internal" in str(element):
+            list_of_responses.append(str(element))
+    return list_of_responses
 
 
-if __name__ == '__main__':
-    while True:
-        try:
-            execute()
-        except (KeyboardInterrupt, SystemExit):
-            COMMENTS_DB_CONN.commit()
-            raise
-        except:
-            COMMENTS_DB_CONN.commit()
-            log(traceback.format_exc())
+def page_to_parse(url):
+    """Method used to open given url and return the received body (UTF-8 encoding)."""
+    scheme, netloc, path, query, fragment = parse.urlsplit(url)
+    path = parse.quote(path)
+    url = parse.urlunsplit((scheme, netloc, path, query, fragment))
+    request = Request(url)
+    request.add_header("User-Agent", "Mozilla/5.0")
+    response = urlopen(request)
+    return response.read().decode("UTF-8")
+
+
+def pages_for_category(category_name):
+    """Method that returns a list of page endings for a given Wiki category."""
+    category_name = category_name.replace(" ", "_")
+    continue_code = ""
+    continue_bool = 1
+    output = []
+    while continue_bool == 1:
+        json_response = page_to_parse(URL_BEGINNING + URL_START+ continue_code + URL_END + category_name)
+
+        parsed_json = json.loads(json_response)
+        for categorymembers in parsed_json["query"]["categorymembers"]:
+            for value in categorymembers.values():
+                if isinstance(value, str) and '/' not in value:
+                    output.append(value.replace(" ", "_"))
+                    outlength = len(output)
+        if 'continue' in parsed_json:
+            continue_bool = 1
+            continue_code = '&cmcontinue=' + parsed_json["continue"]["cmcontinue"]
+        else:
+            continue_bool = 0
+    
+    return output
+
+def response_text_from_element(element):
+    """Method that returns a key for a given element taken from parsed html body."""
+    title = re.findall(r'title="([^"]*)"', element)
+    title = ''.join(title)
+    title = title.split(' - ')
+    title.pop(0)
+    title = ''.join(title)
+    key = re.sub(r'\.mp3$', '', title)
+    key = key.lower()
+    key = key.replace(r"…", "...")
+    #key = key.replace(r"!", "")
+    key = key.replace(r"–", "")
+    #key = clean_key(key)
+    return key
+
+
+def clean_key(key):
+    """Method that cleans the given key, so that it is a lowercase string with
+    no dots or exclamation marks ending the string. All html tags are removed as well."""
+    # This is currently unused. May be implemented in the future.
+    if "<i>" in key:
+        start_index = key.find("<i>")
+        end_index = key.rfind("</i>") + 4
+        key = key.replace(key[start_index:end_index], "")
+
+    if "(" in key and ")" in key:
+        start_index = key.find("(")
+        end_index = key.rfind(")") + 1
+        key = key.replace(key[start_index:end_index], "")
+    key = key.strip()
+    try:
+        if key[-1] in [".", "!"]:
+            key = key[:-1]
+    except IndexError:
+        print("IndexError in: " + key)
+    if key[-2:] == "--":
+        key = key[:-2]
+    key = key.replace("  ", " ")
+    key = key.strip()
+    key = key.lower()
+    return key
+
+
+def value_from_element(element):
+    """Method that returns a value (url to the response) for a given element taken
+    from parsed html body."""
+    start_index = element.find("href=\"") + 6
+    end_index = element.find("\" title")
+    value = element[start_index:end_index]
+    return value
+
+
+def short_hero_name_from_url(url):
+    """Method that returns a short hero name for the given url
+    (taken from the filename on the Wiki server)."""
+    heroname = ''.join(url)
+    if "<a" in url:
+        heroname = re.findall(r'title="([^"]*)"', url)
+        heroname = heroname[0]
+        heroname = re.sub(r'\.mp3$', '', heroname)
+
+    if "File" in url:
+        heroname = heroname.replace("File:", "")
+
+    if "._" in heroname[:4]:
+        heroname = heroname.split('._')
+        heroname = heroname[1]
+
+    if ". " in heroname[:4]:
+        heroname = heroname.split('. ')
+        heroname = heroname[1]
+
+    if "Taunt" in url:
+        heroname = heroname.replace("Taunt.", "")
+
+    heroname = heroname.split('-')
+    heroname = heroname[0]
+    heroname = heroname.replace("_", " ")
+    heroname = heroname.strip()
+    return heroname
+
+def short_hero_name_from_actual_url(actualurl):
+    """Method that returns a short hero name for the given url
+    (taken from the filename on the Wiki server)."""
+    actualurl = actualurl.split('/')
+    actualurl = actualurl[7]
+    actualurl = short_hero_name_from_url(actualurl)
+    return actualurl
+
+
+def ellipsis_to_three_dots(dictionary):
+    """Method that replaces all ellipsis (…) with three dots (...) in the dictionary keys."""
+    newdict = {}
+
+    for key in dictionary:
+        newdict[key] = dictionary[key]
+        if "…" in key:
+            new = key.replace("…", "...")
+            newdict[new] = newdict.pop(key)
+
+    return newdict
+
+#generate_dictionaries(properties.RESPONSES_FILENAME, properties.HEROES_FILENAME, properties.SHITTY_WIZARD_FILENAME)
+#dictionary = dictionary_from_file(properties.RESPONSES_FILENAME)
